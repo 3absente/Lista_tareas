3ac8cc6434be648357670e355beb1090dc8d3226
3absente <victordiazracero@gmail.com>
2017-10-12 14:31:08 +0200

    finalizacion del proyecto
    Proyecto finalizado con el método borrar tarea

--- index.js
+++ index.js
@@ -8,8 +8,21 @@ app.use(session({secret:'nodejs'}));
 app.use(bodyParser.urlencoded({extended:false}));
 app.set('view engine', 'ejs');
 
+var tareas = [];
+
 app.get('/', function(req,res){
-	res.render('formulario.ejs',{})
+	res.render('formulario.ejs',{tareas : tareas});
+})
+
+app.post('/adicionar', function(req,res){
+	var tarea = req.body.nuevaTarea;
+	tareas.push(tarea);
+	res.redirect('/');
+})
+
+app.get('/borrar/:id', function(req,res){
+	tareas.splice(req.params.id, 1);
+	res.redirect('/');
 })
 
 app.listen(3000, function(){
