0ab7f1a66d1c54b6c54ffccb604e9ebc151e8075
3absente <victordiazracero@gmail.com>
2017-10-12 14:01:28 +0200

    - creación del servidor
    Se ha creado el servidor de la web en el archivo index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var session = require('cookie-session');
+
+var app = express();
+
+app.use(session({secret:'nodejs'}));
+app.use(bodyParser.urlencoded({extended:false}));
+app.set('view engine', 'ejs');
+
+app.listen(3000, function(){
+	console.log("Corriendo servidor en el puerto 3000");
+})
