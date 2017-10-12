80ed2f640dd1ba492918267c48441a6e6dc67827
3absente <victordiazracero@gmail.com>
2017-10-12 14:10:32 +0200

    creación de plantilla
    - Creada plantilla HTML para la interfaz gráfica de nuestra aplicación

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(session({secret:'nodejs'}));
 app.use(bodyParser.urlencoded({extended:false}));
 app.set('view engine', 'ejs');
 
+app.get('/', function(req,res){
+	res.render('formulario.ejs',{})
+})
+
 app.listen(3000, function(){
 	console.log("Corriendo servidor en el puerto 3000");
 })
